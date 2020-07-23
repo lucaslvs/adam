f788c2c77d0a1facd58797f05d3d371f8452a5ed
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-07-23 20:24:54 -0300

    fix Communication tests

--- test/adam/communication_test.exs
+++ test/adam/communication_test.exs
@@ -1,6 +1,8 @@
 defmodule Adam.CommunicationTest do
   use Adam.DataCase
 
+  import Adam.Factory
+
   alias Adam.Communication
 
   describe "transmissions" do
@@ -18,23 +20,14 @@ defmodule Adam.CommunicationTest do
     }
     @invalid_attrs %{label: nil, scheduled_at: nil, state: nil}
 
-    def transmission_fixture(attrs \\ %{}) do
-      {:ok, transmission} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Communication.create_transmission()
-
-      transmission
-    end
-
     test "list_transmissions/0 returns all transmissions" do
-      transmission = transmission_fixture()
+      transmission = insert(:transmission)
       assert Communication.list_transmissions() == [Transmission.load_states(transmission)]
     end
 
     test "get_transmission!/1 returns the transmission with given id" do
-      transmission = transmission_fixture()
-      assert Communication.get_transmission!(transmission.id) == transmission
+      transmission = insert(:transmission)
+      assert Communication.get_transmission!(transmission.id) == Transmission.load_states(transmission)
     end
 
     test "create_transmission/1 with valid data creates a transmission" do
@@ -51,7 +44,7 @@ defmodule Adam.CommunicationTest do
     end
 
     test "update_transmission/2 with valid data updates the transmission" do
-      transmission = transmission_fixture()
+      transmission = insert(:transmission)
 
       assert {:ok, %Transmission{} = transmission} =
                Communication.update_transmission(transmission, @update_attrs)
@@ -62,22 +55,22 @@ defmodule Adam.CommunicationTest do
     end
 
     test "update_transmission/2 with invalid data returns error changeset" do
-      transmission = transmission_fixture()
+      transmission = insert(:transmission)
 
       assert {:error, %Ecto.Changeset{}} =
                Communication.update_transmission(transmission, @invalid_attrs)
 
-      assert transmission == Communication.get_transmission!(transmission.id)
+      assert Transmission.load_states(transmission) == Communication.get_transmission!(transmission.id)
     end
 
     test "delete_transmission/1 deletes the transmission" do
-      transmission = transmission_fixture()
+      transmission = insert(:transmission)
       assert {:ok, %Transmission{}} = Communication.delete_transmission(transmission)
       assert_raise Ecto.NoResultsError, fn -> Communication.get_transmission!(transmission.id) end
     end
 
     test "change_transmission/1 returns a transmission changeset" do
-      transmission = transmission_fixture()
+      transmission = insert(:transmission)
       assert %Ecto.Changeset{} = Communication.change_transmission(transmission)
     end
   end
