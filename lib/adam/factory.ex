9b774db5023656b04171ca270b3e13bca48c8292
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-07-25 05:08:35 -0300

    apply formatter

--- lib/adam/factory.ex
+++ lib/adam/factory.ex
@@ -2,7 +2,9 @@ defmodule Adam.Factory do
   use ExMachina.Ecto, repo: Adam.Repo
 
   alias Adam.Communication.Transmission
+  alias Adam.Information.TransmissionState
 
+  @doc false
   def transmission_factory do
     %Transmission{
       label: "label",
@@ -11,8 +13,14 @@ defmodule Adam.Factory do
     }
   end
 
+  @doc false
   def transmission_state_factory(attrs) do
-    %{transmission: %Transmission{} = transmission} = attrs
-    Ecto.build_assoc(transmission, :states, attrs)
+    case attrs do
+      %{transmission: %Transmission{} = transmission} ->
+        Ecto.build_assoc(transmission, :states, attrs)
+
+      _ ->
+        struct!(TransmissionState, attrs)
+    end
   end
 end
