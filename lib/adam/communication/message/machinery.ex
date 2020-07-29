e1d7a1febed6cadb416320bdda015b4ae08c4129
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-07-29 19:09:09 -0300

    log Message transitions

--- lib/adam/communication/message/machinery.ex
+++ lib/adam/communication/message/machinery.ex
@@ -22,4 +22,16 @@ defmodule Adam.Communication.Message.Machinery do
     }
 
   require Logger
+
+  alias Adam.Communication.Message
+
+  @doc false
+  def log_transition(%Message{id: id} = message, next_state) do
+    from = "message_id: #{id}"
+    to = "to: #{next_state}"
+
+    Logger.info("[#{__MODULE__}] Performing state transition of #{from} #{to}")
+
+    message
+  end
 end
